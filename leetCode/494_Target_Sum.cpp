4257bb825ae548dc94b967c60ce11b1f380c77c7
boruoyihao <zhaolizhen2009@gmail.com>
2017-05-23 01:59:17 +0800

    494 Target Sum
    Signed-off-by: boruoyihao <zhaolizhen2009@gmail.com>

--- leetCode/494_Target_Sum.cpp
+++ leetCode/494_Target_Sum.cpp
@@ -54,6 +54,8 @@ public:
 };
 #endif
 
+#if 0
+//265ms
 class Solution {
 
 public:
@@ -63,26 +65,30 @@ public:
 			return 0;
 		}
 		map<int,int>m1;
-		m1[0]=0;
+		m1[0]=1;
 		map<int,int>m2;
 		
 
 		for(int i=0;i<nums.size();i++){
 			for(map<int,int>::iterator it=m1.begin();it!=m1.end();it++){
-				if(m2.find((it->first)+nums[i])==-1){
-					m2[it->first+nums[i]]=1;	
+				if(m2.find((it->first)+nums[i])==m2.end()){
+					m2[it->first+nums[i]]=it->second;	
 				}else{
-					m2[it->first+nums[i]]++;
+					m2[it->first+nums[i]]=m2[it->first+nums[i]]+it->second;
 				}
 
-				if(m2.find((it->first)-nums[i])==-1){
-					m2[it->first-nums[i]]=1;
+				if(m2.find((it->first)-nums[i])==m2.end()){
+					m2[it->first-nums[i]]=it->second;
 				}else{
-					m2[it->first-nums[i]]++;
+					m2[it->first-nums[i]]=m2[it->first-nums[i]]+it->second;
 				}
 			}
 			m1.clear();
-			//m1.insert(m2);
+			for(map<int,int>::iterator it=m2.begin();it!=m2.end();it++){
+				m1[it->first]=it->second;
+				//cout<<it->first<<" "<<it->second<<" "<<endl;
+			}
+			//cout<<endl;
 			m2.clear();
 		}
 
@@ -91,16 +97,98 @@ public:
 			if(S==it->first){
 				result+=it->second;
 			}
+			//cout<<it->first<<" "<<it->second<<endl;
 		}
 		return result;
     }
 
 };
 
+#endif
+#if 0
+class Solution {
+
+public:
+
+    int findTargetSumWays(vector<int>& nums, int S) {
+
+        vector<int>v(2000,0);
+
+		v[0]=1;
+		for(int i=0;i<nums.size();i++){
+			for(int j=0;j<2000;j++){
+				if(v[j]==0||v[j]==-1){
+					continue;
+				}
+				int plus,minus;
+				if(j<1000){
+					plus=j+nums[i];
+					minus=j-nums[i];
+				}else{
+					plus=1000-j+nums[i];
+					minus=1000-j-nums[i];
+				}
+				
+				cout<<j<<"="<<v[j]<<" "<<plus<<" "<<minus<<"++"<<endl;
+				if(plus>0){
+					v[plus]+=v[j];
+				}else{
+					v[0-plus+1000]+=v[j];
+				}
+
+				if(minus>0){
+					v[minus]+=v[j];
+				}else{
+					v[0-minus+1000]+=v[j];
+				}
+
+                		   
+
+				v[j]=-1;
+
+			}
+			cout<<endl;
+		}
+
+		for(int i=0;i<50;i++){
+			cout<<v[i]<<" ";
+		}
+		cout<<endl;
+		return S>0?v[S]:v[0-S+1000];
+
+    }
+
+};
+
+#endif
+
+class Solution {
+
+public:
+
+    int findTargetSumWays(vector<int>& nums, int S) {      
+		int sum=0;
+		for(int i=0;i<nums.size();i++){
+			sum+=nums[i];
+		}
+		return sum<S||(S+sum)&1?0:findTargetSum(nums,(S+sum)>>1);
+    }
+
+	int findTargetSum(vector<int>& nums,int sum){
+		vector<int> dp(sum+1,0);
+		dp[0]=1;
+		for(auto no:nums){
+			for(int i=sum;i>=no;i--){
+				dp[i]+=dp[i-no];
+			}
+		}
+        return dp[sum];
+	}
+};
 int main(){
 	Solution*s=new Solution;
 	int array[]={1, 1, 1, 1, 1};
 	vector<int>v(array,array+5);
-	cout<<s->findTargetSumWays(v,1)<<endl;
+	cout<<s->findTargetSumWays(v,3)<<endl;
 	return 0;
 }
