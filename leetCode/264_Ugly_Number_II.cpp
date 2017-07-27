317910509a2ef37cc10d504124cf7f6721a43744
zhaolizhen <zhaolizhen@xiaomi.com>
2017-07-27 21:18:28 +0800

    ugly num
    Signed-off-by: zhaolizhen <zhaolizhen@xiaomi.com>

--- leetCode/264_Ugly_Number_II.cpp
+++ leetCode/264_Ugly_Number_II.cpp
@@ -7,17 +7,40 @@ Note that 1 is typically treated as an ugly number, and n does not exceed 1690.
 */
 
 #include "common.h"
+using namespace std;
 
 class Solution {
 public:
     int nthUglyNumber(int n) {
-        int factor2=1;
-        int factor3=1;
-        int factor5=1;
-        int start=1;
-        for(int i=0;i<n;i++){
-           factor2*2
-        }    
+        if(n<=0){
+           return 0;
+        }
+        int *mem=new int[n];
+        mem[0]=1;
+        int factor2=0;
+        int factor3=0;
+        int factor5=0;
+        int next=1;
+        while(next<n){
+           int min=min3(mem[factor2]*2,mem[factor3]*3,mem[factor5]*5);
+           mem[next]=min;
+           while(mem[factor2]*2<=min){
+                factor2++;
+           }
+           while(mem[factor3]*3<=min){
+                factor3++;
+           }
+           while(mem[factor5]*5<=min){
+                factor5++;
+           }
+           next++;
+        }
+       return mem[n-1];    
+    }
+
+    int min3(int no1,int no2,int no3){
+
+	return no1>no2?(no2>no3?no3:no2):(no1>no3?no3:no1);
     }
 };
 
@@ -26,6 +49,6 @@ int main(){
 
     Solution *s =new Solution;
     cout<<s->nthUglyNumber(3)<<endl;
-    cout<<s-nthUglyNumber(5)<<endl;
+    cout<<s->nthUglyNumber(10)<<endl;
     return 0;
 }
