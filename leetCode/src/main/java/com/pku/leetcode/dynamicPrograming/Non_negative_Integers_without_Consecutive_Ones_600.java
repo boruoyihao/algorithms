5960558b6e4bb5cf429fec542e6e74433049884d
zhaolizhen [赵立振] <zhaolizhen@xiaomi.com>
2018-05-19 15:56:50 +0800

    Non_negative_Integers_without_Consecutive_Ones_600
    Signed-off-by: zhaolizhen [赵立振] <zhaolizhen@xiaomi.com>

--- leetCode/src/main/java/com/pku/leetcode/dynamicPrograming/Non_negative_Integers_without_Consecutive_Ones_600.java
+++ leetCode/src/main/java/com/pku/leetcode/dynamicPrograming/Non_negative_Integers_without_Consecutive_Ones_600.java
@@ -4,6 +4,9 @@ package com.pku.leetcode.dynamicPrograming;
  * Created by zhaolizhen on 18-5-15.
  */
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 /**
  * Given a positive integer n, find the number of non-negative integers less than or equal to n,
  * whose binary representations do NOT contain consecutive ones.
@@ -29,10 +32,92 @@ public class Non_negative_Integers_without_Consecutive_Ones_600 {
 
     }
 
+    //Memory Limit Exceeded 27/527 test cases passed
+    //consutruct a tree to solve this problem
+    private static class Solution1 {
+        public int findIntegers(int num) {
+            if(num<=0){
+                return 0;
+            }
 
+            int value=1;
+            while(value<=num){
+                value=value<<1;
+            }
+
+            int mem[]=new int[value*2];
+
+            mem[0]=0;
+            Queue<Node> queue=new ArrayDeque<>(value*2);
+            queue.add(new Node(0,0));
+            while(!queue.isEmpty()){
+                Node previous=queue.poll();
+                if(previous.index*2+2>=mem.length){
+                    break;
+                }
+                if(previous.value==0){
+                    mem[previous.index*2+1]=0;
+                    mem[previous.index*2+2]=1;
+                }else if(previous.value==1){
+                    mem[previous.index*2+1]=0;
+                    mem[previous.index*2+2]=2;
+                }else{
+                    mem[previous.index*2+1]=2;
+                    mem[previous.index*2+2]=2;
+                }
+
+                queue.add(new Node(previous.index*2+1,mem[previous.index*2+1]));
+                queue.add(new Node(previous.index*2+2,mem[previous.index*2+2]));
+            }
+
+            int sum=0;
+            for(int j=value-1;j<mem.length;j++){
+                if(mem[j]!=2&&(j-(value-1))<=num){
+                    sum++;
+                }
+            }
+//            System.out.println(Arrays.toString(mem));
+            return sum;
+        }
+
+        private static class Node{
+            int index;
+            int value;
+            public Node(int index,int value){
+                this.index=index;
+                this.value=value;
+            }
+        }
+
+    }
+
+//https://leetcode.com/problems/non-negative-integers-without-consecutive-ones/solution/
     private static class Solution {
         public int findIntegers(int num) {
-            return 0;
+            int f[]=new int[32];
+            f[0]=1;
+            f[1]=2;
+            for(int i=2;i<32;i++){
+                f[i]=f[i-2]+f[i-1];
+            }
+
+            int sum=0;
+            boolean previous=false;
+            int i=30;
+            while(i>=0){
+                if((num&(1<<i))!=0){
+                    sum+=f[i];
+                    if(previous){
+                        sum--;
+                        break;
+                    }
+                    previous=true;
+                }else{
+                    previous=false;
+                }
+                i--;
+            }
+            return sum+1;
         }
     }
 }
